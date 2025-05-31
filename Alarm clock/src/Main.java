8bb5920dea0b25e28b71b216666dc43c3745e280
john muriungi <151134779+John-Muriungi@users.noreply.github.com>
2025-05-31 18:26:40 +0300

    update

--- Alarm clock/src/Main.java
+++ Alarm clock/src/Main.java
@@ -1,5 +1,36 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       // java Alarm clock
+
+        Scanner scanner =new Scanner(System.in);
+        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime=null;
+        while(alarmTime==null){
+            try{
+                System.out.print("Enter an alarm time(HH:MM:SS): ");
+                String inputTime=scanner.nextLine();
+
+                alarmTime=LocalTime.parse(inputTime,formatter);
+                System.out.println("Alarm is set for: "+alarmTime);
+            }
+            catch(DateTimeParseException e){
+                System.out.println("Wrong input! Time should be in HH:MM:SS");
+
+
+            }
+
+        }
+
+        AlarmClock alarmClock=new AlarmClock(alarmTime);
+        Thread alarmThread=new Thread(alarmClock);
+        alarmThread.start();
+
+        scanner.close();
     }
 }
